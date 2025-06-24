cd1ff9f1260fcb72de806a4097bf08879c85370b
sonveersingh1990 <sonveers.singh1990@gmail.com>
2025-06-24 12:42:34 +0530

    added ec2 instance

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
    region = "ap-south-1"
 }
+
+resource "aws_instance" "web_app" {
+   ami = "demo_instance_id"
+   instance_type = "t2.micro"
+
+   tags = {
+    name = web_app
+   }
+}
