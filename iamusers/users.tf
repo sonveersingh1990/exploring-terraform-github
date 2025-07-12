dab16ffe0ce83b27228c3b78cd832cf12da7f878
sonveersingh1990 <sonveers.singh1990@gmail.com>
2025-07-12 16:36:57 +0530

    added user attribute

--- iamusers/users.tf
+++ iamusers/users.tf
@@ -1,3 +1,26 @@
 resource "aws_iam_user" "github2" {
   name = github2
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "github2" {
+  user = aws_iam_user.github2.name
+}
+
+data "aws_iam_policy_document" "github2_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "github2_ro" {
+  name   = "test"
+  user   = aws_iam_user.lb.name
+  policy = data.aws_iam_policy_document.github2_ro.json
 }
