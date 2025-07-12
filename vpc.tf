5c8fd72fff5baab35c80bd61801c03b327638266
sonveersingh1990 <sonveers.singh1990@gmail.com>
2025-07-12 15:28:03 +0530

    added vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,22 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
